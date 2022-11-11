f1cb2cd24d5074ee18a9af0f955d8ab64184af0c
Carlos Eduardo <carlospintorossy07@gmail.com>
2022-11-11 11:48:01 -0400

    🔧 configuration path Mapping

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,25 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ['babel-preset-expo'],
+    Plugins: [
+      [
+        'module-resolver',
+        {
+          root: ['./src'],
+          extentions: [
+            '.ts',
+            '.tsx',
+            '.js',
+            '.json'
+          ],
+          alias: {
+            '@components': './src/components',
+            '@screens': './src/screens',
+            '@assets': './src/assets',
+          }
+        }
+      ]
+    ]
   };
 };
