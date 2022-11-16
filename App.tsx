560c13e0cff36e026f25a5e4e0f96ce1e93f8455
Carlos Eduardo <carlospintorossy07@gmail.com>
2022-11-16 11:23:46 -0400

    🔧 add config Firebase

--- App.tsx
+++ App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { View } from 'react-native';
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans"
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display"
 import AppLoading from 'expo-app-loading';
