6851c11832286ba0318c855f8831d785fa6ad6ef
Carlos Eduardo <carlospintorossy07@gmail.com>
2022-12-11 20:23:03 -0400

    ✨ create component Input

--- App.tsx
+++ App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useFonts, DMSans_400Regular } from "@expo-google-fonts/dm-sans"
 import { DMSerifDisplay_400Regular } from "@expo-google-fonts/dm-serif-display"
+import { StatusBar } from 'expo-status-bar'
 import AppLoading from 'expo-app-loading';
 import { ThemeProvider } from "styled-components/native"
 
@@ -20,6 +21,7 @@ export default function App() {
 
   return (
     <ThemeProvider theme={theme}>
+      <StatusBar style='light' translucent backgroundColor='transparent' />
       <SingIn />
     </ThemeProvider>
   );
--- src/screens/SingIn/index.tsx
+++ src/screens/SingIn/index.tsx
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Input } from '../../components/Input';
 import * as S from './styles'
 
 export function SingIn() {
     return (
         <S.Container>
 
+            <Input
+                placeholder='E-mail'
+                type='secundary'
+                autoCorrect={false}
+                autoCapitalize='none'
+            />
+            <Input
+                placeholder='Senha'
+                type='secundary'
+                secureTextEntry
+            />
         </S.Container>
     )
 }
